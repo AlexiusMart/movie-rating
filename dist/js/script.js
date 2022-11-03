32567c97ddbeed12d3f4c3a23d3ae9d609c8e843
iStre1ok <am-nf@yandex.ru>
2022-11-03 21:55:32 +0300

    preparation

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const data = [];
+
+data[0] = prompt('Как Ваше имя?', '');
+data[1] = prompt('Какая Ваша Фамилия?', '');
+data[2] = prompt('Сколько Вам лет?', '');
+
+
+console.log(2*4 == '8')
